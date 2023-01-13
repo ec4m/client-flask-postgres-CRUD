9e47471e8ab9f1e74f72bfdd40ea484af23cd168
ec4m <ec.angaritas@gmail.com>
2023-01-13 12:22:07 -0500

    metodos get_product y add_product listos

--- src/models/ProductModel.py
+++ src/models/ProductModel.py
@@ -12,9 +12,45 @@ class ProductModel:
         resultset = cursor.fetchall()
         for row in resultset:
           product = Product(row[0], row[1], row[2], row[3])
-          products.append(product)
+          products.append(product.to_JSON())
       conn.close()
       return products
       
+    except Exception as ex:
+      raise Exception(ex)
+
+
+  @classmethod
+  def get_product(self, id):
+    try:
+      conn = get_connection()
+      with conn.cursor() as cursor:
+        cursor.execute("SELECT id, name, description, price FROM product WHERE id=%s", (id,))
+        row = cursor.fetchone()
+        product = None
+        if row != None:
+          product = Product(row[0], row[1], row[2], row[3])
+          product = product.to_JSON()
+        else:
+          product = None
+      conn.close()
+      return product
+
+    except Exception as ex:
+      raise Exception(ex)
+
+
+  @classmethod
+  def add_product(self, product):
+    try:
+      conn = get_connection()
+      with conn.cursor() as cursor:
+        cursor.execute("""INSERT INTO product (id, name, description, price)
+                          VALUES (%s, %s, %s, %s)""", (product.id, product.name, product.description, product.price))
+        affected_rows = cursor.rowcount
+        conn.commit()
+      conn.close()
+      return affected_rows
+
     except Exception as ex:
       raise Exception(ex)
--- src/routes/Product.py
+++ src/routes/Product.py
@@ -16,3 +16,36 @@ def get_products():
     return jsonify(products)
   except Exception as ex:
     return jsonify({'message': str(ex)})
+
+
+@product.route('/<id>')
+def get_product(id):
+  try:
+    product = ProductModel.get_product(id)
+    if product != None:
+      return jsonify(product)
+    else:
+      return jsonify({'message': 'Product not found'})
+    
+  except Exception as ex:
+    return jsonify({'message': str(ex)})
+
+
+@product.route('/add', methods=['POST'])
+def add_product():
+  try:
+    id = uuid.uuid4()
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    product = Product(str(id), name, description, price)
+    affected_row = ProductModel.add_product(product)
+
+    if affected_row != None:
+      return jsonify(product.id)
+    else: 
+      return jsonify({'message': 'Error on insert'})
+
+    
+  except Exception as ex:
+    return jsonify({'message': str(ex)})
